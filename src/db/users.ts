46e0482e2a72a6672aa3809b49566ad79695b3db
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-05-20 14:23:04 +0700

    Enhance Mongoose schema and database operations
    - Add error handling to database operations for better error management
    - Utilize async/await for cleaner asynchronous code
    - Rename getUsersByEmail to getUserByEmail for consistency
    - Ensure all functions return promises for consistency and better async handling

--- src/db/users.ts
+++ src/db/users.ts
@@ -11,12 +11,60 @@ const UserSchema = new mongoose.Schema({
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
-export const getUsers = () => UserModel.find();
-export const getUsersByEmail = (email: string) => UserModel.findOne({ email });
-export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
-    'authentication.sessionToken': sessionToken
-});
-export const getUserById = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
-export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
-export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+
+export const getAllUsers = async () => {
+    try {
+        return await UserModel.find();
+    } catch (error) {
+        throw new Error(`Error getting users: ${error}`);
+    }
+};
+
+export const getUserByEmail = async (email: string) => {
+    try {
+        return await UserModel.findOne({ email });
+    } catch (error) {
+        throw new Error(`Error getting user by email: ${error}`);
+    }
+};
+
+export const getUserBySessionToken = async (sessionToken: string) => {
+    try {
+        return await UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+    } catch (error) {
+        throw new Error(`Error getting user by session token: ${error}`);
+    }
+};
+
+export const getUserById = async (id: string) => {
+    try {
+        return await UserModel.findById(id);
+    } catch (error) {
+        throw new Error(`Error getting user by ID: ${error}`);
+    }
+};
+
+export const createUser = async (values: Record<string, any>) => {
+    try {
+        const user = await UserModel.create(values);
+        return user.toObject();
+    } catch (error) {
+        throw new Error(`Error creating user: ${error}`);
+    }
+};
+
+export const deleteUserById = async (id: string) => {
+    try {
+        return await UserModel.findOneAndDelete({ _id: id });
+    } catch (error) {
+        throw new Error(`Error deleting user by ID: ${error}`);
+    }
+};
+
+export const updateUserById = async (id: string, values: Record<string, any>) => {
+    try {
+        return await UserModel.findByIdAndUpdate(id, values);
+    } catch (error) {
+        throw new Error(`Error updating user by ID: ${error}`);
+    }
+};
