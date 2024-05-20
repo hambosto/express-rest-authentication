739d47bd65a8b229202a7dda8708f07048d4393c
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-05-20 14:21:34 +0700

    Keep SECRET_KEY hardcoded in code for simplicity
    - Define SECRET_KEY directly in the code
    - Maintain simplicity by avoiding the use of environment variables
    - Note the potential security implications of this approach

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,13 @@
 import crypto from 'crypto';
 
+// Hardcoded secret key?
 const SECRET_KEY = "Got-My-Key?";
 
-export const random = () => crypto.randomBytes(128).toString('base64');
-export const authentication = (salt: string, password: string) => {
-    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET_KEY).digest('hex')
-}
+export const random = (): string => crypto.randomBytes(128).toString('base64');
 
+export const authentication = (salt: string, password: string): string => {
+    return crypto
+        .createHmac('sha256', [salt, password].join('/'))
+        .update(SECRET_KEY)
+        .digest('hex');
+};
